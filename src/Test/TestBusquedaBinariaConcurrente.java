b6b69d5fb70ca3d4d8cfe64b23748468e8d86c45
MarcosTambosco <marcostambosco@gmail.com>
2025-06-12 19:42:10 -0300

    Correccion de la clase BusquedaBinariaSecuencial

--- src/Test/TestBusquedaBinariaConcurrente.java
+++ src/Test/TestBusquedaBinariaConcurrente.java
@@ -1,110 +1,119 @@
 package Test;
 
+import java.util.Arrays;
+import java.util.Random;
 
-class BusquedaBinariaThread extends Thread {
-    private int[] arreglo;       // Arreglo sobre el que se realiza la búsqueda
-    private int valorBuscado;    // Valor que se desea encontrar
-    private int inicio;          
-    private int fin;             
-    private int resultado = -1;  // Variable para guardar el índice donde se encuentra el valor (-1 si no se encuentra)
+// Clase para realizar busqueda binaria concurrente usando threads
+class BusquedaBinariaConcurrente {
 
-    // Constructor
-    public BusquedaBinariaThread(int[] arreglo, int valorBuscado, int inicio, int fin) {
-        this.arreglo = arreglo;
-        this.valorBuscado = valorBuscado;
-        this.inicio = inicio;
-        this.fin = fin;
-    }
+    // Clase interna que extiende Thread y realiza busqueda binaria en un rango del arreglo
+    static class BusquedaThread extends Thread {
+        private int[] arreglo;       // Arreglo completo donde se buscara
+        private int numeroBuscado;   // Numero que queremos encontrar
+        private int inicio;          // Indice inicial del rango para este thread
+        private int fin;             // Indice final del rango para este thread
+        private int resultado = -1;  // Resultado: posicion encontrada o -1 si no encontro
 
-    // Inicializo el hilo
-    public void run() {
-        int i = inicio;
-        int f = fin;
+        // Constructor que recibe arreglo, numero buscado y rango donde buscar
+        public BusquedaThread(int[] arreglo, int numeroBuscado, int inicio, int fin) {
+            this.arreglo = arreglo;
+            this.numeroBuscado = numeroBuscado;
+            this.inicio = inicio;
+            this.fin = fin;
+        }
 
-        // Bucle de búsqueda binaria tradicional
-        while (i <= f) {
-            int medio = (i + f) / 2;
+        // Metodo para obtener el resultado despues de ejecutar el thread
+        public int getResultado() {
+            return resultado;
+        }
 
-            // Si el elemento en la posición medio es el valor buscado, lo guarda y termina
-            if (arreglo[medio] == valorBuscado) {
-                resultado = medio;
-                return;
+        // Metodo run que se ejecuta cuando inicia el thread
+        @Override
+        public void run() {
+            int bajo = inicio; // indice inicial para la busqueda binaria
+            int alto = fin;    // indice final para la busqueda binaria
+
+            // Mientras el indice inicial sea menor o igual al final
+            while (bajo <= alto) {
+                int medio = (bajo + alto) / 2;     // calculamos el punto medio
+                int numMedio = arreglo[medio];     // obtenemos el valor en la posicion media
+
+                if (numMedio == numeroBuscado) {  // si encontramos el numero buscado
+                    resultado = medio;             // guardamos la posicion encontrada
+                    return;                       // terminamos el thread
+                }
+                if (numeroBuscado < numMedio) {  // si el numero buscado es menor que el medio
+                    alto = medio - 1;             // buscamos en la mitad izquierda
+                } else {                         // si es mayor
+                    bajo = medio + 1;             // buscamos en la mitad derecha
+                }
             }
 
-            // Si el valor buscado es menor, buscar en la mitad izquierda
-            if (valorBuscado < arreglo[medio]) {
-                f = medio - 1;
-            } else {
-                // Si es mayor, buscar en la mitad derecha
-                i = medio + 1;
-            }
+            resultado = -1; // si no se encontro el numero en el rango, resultado es -1
         }
     }
 
-    //Get
-    public int getResultado() {
-        return resultado;
-    }
-}
-
-// Clase principal donde se prueba la búsqueda binaria concurrente
-public class TestBusquedaBinariaConcurrente {
-
-    
-    private static final int TAM = 1000000; 
-
+    // Metodo principal donde se ejecuta el programa
     public static void main(String[] args) {
-       
-        
-        int[] datos = new int[TAM];
-        for (int i = 0; i < TAM; i++) {
-            datos[i] = i + 1;
+        Random random = new Random(); // instancia para generar numeros aleatorios
+
+        int numeroABuscar = 25;       // numero que queremos buscar en el arreglo
+        int tamanioArray = 10;        // tamaño del arreglo a generar
+
+        int[] arregloDeNumeros = new int[tamanioArray]; // creamos el arreglo
+
+        // llenamos el arreglo con numeros aleatorios entre 0 y 50
+        for (int i = 0; i < arregloDeNumeros.length; i++) {
+            arregloDeNumeros[i] = random.nextInt(51);
         }
 
-       
-        int valor = 5;
+        Arrays.sort(arregloDeNumeros); // ordenamos el arreglo para busqueda binaria
 
-        // División del arreglo en dos mitades para asignar a cada hilo
-        int mitad = datos.length / 2;
+        System.out.println(Arrays.toString(arregloDeNumeros)); // mostramos arreglo en consola
+        System.out.println("El numero a buscar es: " + numeroABuscar); // mostramos numero a buscar
 
-        // Creación de dos hilos, uno para la primera mitad, otro para la segunda mitad
-        BusquedaBinariaThread hilo1 = new BusquedaBinariaThread(datos, valor, 0, mitad - 1);
-        BusquedaBinariaThread hilo2 = new BusquedaBinariaThread(datos, valor, mitad, datos.length - 1);
+        int cantidadThreads = 2; // cantidad de threads para dividir la busqueda
 
-       
-        long tiempoInicio = System.nanoTime();
+        // calculamos tamaño del rango que manejara cada thread
+        int rango = (int) Math.ceil((double) tamanioArray / cantidadThreads);
 
-        // Inicio de ambos hilos
-        hilo1.start();
-        hilo2.start();
+        BusquedaThread[] threads = new BusquedaThread[cantidadThreads]; // arreglo de threads
 
-        // Espera a que ambos hilos terminen su ejecución
+        long tiempoInicio = System.nanoTime(); // tiempo de inicio para medir ejecucion
+
+        // creamos y arrancamos cada thread con su rango definido
+        for (int i = 0; i < cantidadThreads; i++) {
+            int inicio = i * rango; // inicio del rango para este thread
+            int fin = Math.min(inicio + rango - 1, tamanioArray - 1); // fin del rango
+
+            threads[i] = new BusquedaThread(arregloDeNumeros, numeroABuscar, inicio, fin);
+            threads[i].start(); // arrancamos el thread
+        }
+
+        int resultado = -1; // variable para guardar resultado final
+
+        // esperamos que todos los threads terminen
         try {
-            hilo1.join();
-            hilo2.join();
+            for (BusquedaThread thread : threads) {
+                thread.join(); // esperamos que termine el thread
+                if (thread.getResultado() != -1) { // si encontro el numero
+                    resultado = thread.getResultado(); // guardamos la posicion
+                    // aca podriamos romper el ciclo si queremos, pero esperamos a todos
+                }
+            }
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // mostramos error si algo interrumpe la espera
         }
 
-       
-        long tiempoFin = System.nanoTime();
-        long duracion = tiempoFin - tiempoInicio;
+        long tiempoFin = System.nanoTime(); // tiempo final para medir ejecucion
+        long duracion = tiempoFin - tiempoInicio; // calculamos duracion en nanosegundos
 
-        // Determina cuál de los hilos encontró el valor
-        int resultado;
-        if (hilo1.getResultado() != -1) {
-            resultado = hilo1.getResultado();
-        } else {
-            resultado = hilo2.getResultado();
+        if (resultado != -1) { // si se encontro el numero
+            System.out.println("Numero encontrado en la posicion: " + resultado);
+        } else { // si no se encontro
+            System.out.println("Numero no encontrado.");
         }
 
-        if (resultado != -1) {
-            System.out.println("✔ Valor " + valor + " encontrado en la posición: " + resultado);
-        } else {
-            System.out.println("✘ Valor " + valor + " no encontrado.");
-        }
-
-        // Muestra el tiempo de ejecución en nanosegundos
-        System.out.println("⏱ Tiempo de ejecución (concurrente): " + duracion + " nanosegundos");
+        System.out.println("⏱ Tiempo de ejecucion concurrente: " + duracion + " nanosegundos");
     }
 }
--- src/Test/TestBusquedaBinariaSecuencial.java
+++ src/Test/TestBusquedaBinariaSecuencial.java
@@ -1,42 +1,70 @@
 package Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
 // Clase que implementa el algoritmo de busqueda binaria
 class BusquedaBinaria {
 
-    // Metodo privado que realiza la busqueda binaria
-    private static int busquedaBinaria(int[] arregloNumeros, int numeroBuscado) {
-        int bajo = 0;                            // Indice inicial del arreglo
-        int alto = arregloNumeros.length - 1;    // Indice final del arreglo
+	// Metodo privado que realiza la busqueda binaria
+	private static int busquedaBinaria(int[] arregloNumeros, int numeroBuscado) {
+		int bajo = 0; // Indice inicial del arreglo
+		int alto = arregloNumeros.length - 1; // Indice final del arreglo
 
-        // Mientras el índice inicial sea menor o igual al final
-        while (bajo <= alto) {
-            int indiceMedio = (bajo + alto) / 2;           // Calcula el indice del medio
-            int numeroEnMedio = arregloNumeros[indiceMedio]; // Obtiene el numero en la posición media
+		// Mientras el índice inicial sea menor o igual al final
+		while (bajo <= alto) {
+			int indiceMedio = (bajo + alto) / 2; // Calcula el indice del medio
+			int numeroEnMedio = arregloNumeros[indiceMedio]; // Obtiene el numero en la posición media
 
-            if (numeroBuscado == numeroEnMedio) {
-                return indiceMedio;    // Si el numero buscado es igual al número en el medio devuelve el indice
-            }
-            if (numeroBuscado < numeroEnMedio) {
-                alto = indiceMedio - 1; // Si el numero buscado es menor, buscar en la mitad izquierda
-            }
-            if (numeroBuscado > numeroEnMedio) {
-                bajo = indiceMedio + 1; // Si el numero buscado es mayor, buscar en la mitad derecha
-            }
-        }
+			if (numeroBuscado == numeroEnMedio) {
+				return indiceMedio; // Si el numero buscado es igual al número en el medio devuelve el indice
+			}
+			if (numeroBuscado < numeroEnMedio) {
+				alto = indiceMedio - 1; // Si el numero buscado es menor, buscar en la mitad izquierda
+			}
+			if (numeroBuscado > numeroEnMedio) {
+				bajo = indiceMedio + 1; // Si el numero buscado es mayor, buscar en la mitad derecha
+			}
+		}
 
-        return -1; // Si no se encuentra el número, devuelve -1
-    }
+		return -1; // Si no se encuentra el número, devuelve -1
+	}
 
-    // Metodo principal que se ejecuta al correr el programa
-    public static void main(String[] args) {
+	// Metodo principal que se ejecuta al correr el programa
+	public static void main(String[] args) {
 
-        int[] arregloDeNumeros = {2, 3, 6, 8, 9, 13, 20}; // Arreglo de números ordenados
-        
-        int resultado = busquedaBinaria(arregloDeNumeros, 13); // Busca el número 13
+		Random random = new Random();
 
-        if (resultado != -1) {
-            System.out.println("Numero encontrado en la posición: " + resultado);
-        } else {
-            System.out.println("Numero no encontrado.");
-        }
-    }
+		int numeroABuscar = 25;
+		int tamanioArray = 10;
+
+		// Crear un arreglo elementos con el tamaño enviado por parametro
+		int[] arregloDeNumeros = new int[tamanioArray];
+
+		for (int i = 0; i < arregloDeNumeros.length; i++) {
+			arregloDeNumeros[i] = random.nextInt(51); // Genera números de 0 a 50
+		}
+
+		Arrays.sort(arregloDeNumeros); // Ordenar el arreglo para poder aplicar búsqueda binaria
+
+		System.out.println(Arrays.toString(arregloDeNumeros)); // Mostrar el arreglo por consola para verificar
+
+		System.out.println("El numero a buscar es: " + numeroABuscar); // Mostrar el arreglo por consola para verificar
+
+		long tiempoInicio = System.nanoTime(); // Guardar el tiempo al iniciar la operación
+
+		int resultado = busquedaBinaria(arregloDeNumeros, numeroABuscar); // Busca el número "numeroABuscar"
+
+		long tiempoFin = System.nanoTime(); // Guardar el tiempo al finalizar la operación
+
+		long duracion = tiempoFin - tiempoInicio; // Calcular la duración total en nanosegundos
+
+		if (resultado != -1) {
+			System.out.println("Numero encontrado en la posición: " + resultado);
+		} else {
+			System.out.println("Numero no encontrado.");
+		}
+
+		System.out.println("⏱ Tiempo de ejecución: " + duracion + " nanosegundos"); // Mostrar el tiempo en consola
+	}
 }
